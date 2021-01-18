9fddfa28a6b3b4295ae1c301007f23a949ca282e
Karol Gruszczyński <lapera.carlos@gmail.com>
2021-01-18 14:20:42 +0100

    The beginning - first commit of TTT

---
+++ src/main/java/com/kodilla/tictactoe/TicTacToe.java
@@ -0,0 +1,16 @@
+package com.kodilla.tictactoe;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TicTacToe extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+git add *
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
